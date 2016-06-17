91147a6fb0cd152dfaf4a8d3a2b2bc47c73cdc11
Tom Parker <palfrey@tevp.net>
2016-06-17 10:38:50 +0100

    Reset project name for pushing to upstream

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lshift/lein-jruby "0.1.2-SNAPSHOT"
+(defproject lein-jruby "0.1.2-SNAPSHOT"
   :description "Run JRuby commands from Leiningen."
   :dependencies [[org.jruby/jruby-complete "1.6.7"]
                  [org.clojure/tools.logging "0.3.1"]
